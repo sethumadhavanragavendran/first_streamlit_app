c6cc648964e62b0cc2f526c17cc39d808b214e9c
Ragavendran S <119409175+sethumadhavanragavendran@users.noreply.github.com>
2022-11-29 16:06:24 +0530

    Update streamlit_app.py
    Fixed Bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title(' My parents healthy diner app')
 
-streamlit.header('Breakfast menu)
+streamlit.header('Breakfast menu')
 streamlit.text('Spinach, Lettuce and Bread')
 streamlit.text('Bread .. Bread .. Bread ..')
-streamlit.text('Dosa, Idly, Godhi Dosa ....)                 
+streamlit.text('Dosa, Idly, Godhi Dosa ....')                 
