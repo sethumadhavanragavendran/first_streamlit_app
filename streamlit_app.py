5d75399d29c2f5f3fe87736c233b3c3a5b2ae07c
Ragavendran S <119409175+sethumadhavanragavendran@users.noreply.github.com>
2022-12-21 14:49:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,15 +43,19 @@ except URLError as e:
   
 # Normalized List is displayed? 
 # Dataframe object created with the above normalized list
-streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains :")
-streamlit.dataframe(my_data_rows)
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
 
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
 fruit_to_add = streamlit.text_input('What fruit would you like to add?','')
 streamlit.write('The user entered ', fruit_to_add)
 
